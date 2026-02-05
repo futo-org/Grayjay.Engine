0b135cbe174feb94dda4008d3f416282e0f8611e
Koen J <koen@disper.io>
2026-02-05 14:05:14 +0100

    Explicit hide after creation PackageBrowser.

--- Grayjay.Engine/Packages/PackageBrowser.cs
+++ Grayjay.Engine/Packages/PackageBrowser.cs
@@ -81,6 +81,7 @@ namespace Grayjay.Engine.Packages
                 modifyRequests: false,
                 logConsole: false
             ).GetAwaiter().GetResult();
+            win.HideAsync().GetAwaiter().GetResult();
 
             win.OnLoadEnd += url =>
             {
